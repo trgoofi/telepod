538d2fa6237356b57830b8737ff0a0b5eb72c91d
trgoofi <trgoofi@gmail.com>
2015-04-22 13:43:45 +0800

    Fixed build error for hadron on windows.
    gyp call `action` under build directory on windows
    and that cause `deploy.js` behave unexpectedly with relative paths.
    https://github.com/TooTallNate/node-gyp/issues/613

--- electron/hadron/deploy.js
+++ electron/hadron/deploy.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
+var path = require('path');
 
 
-var src = './build/Release/hadron.node';
-var dest = 'hadron-' + process.platform + '-' + process.arch + '.node';
+var src = path.join(__dirname, 'build/Release/hadron.node');
+var dest = path.join(__dirname, 'hadron-' + process.platform + '-' + process.arch + '.node');
 
+console.log('Deploy: [%s] as [%s]', src, dest);
 fs.createReadStream(src).pipe(fs.createWriteStream(dest));
